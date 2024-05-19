efc84dc1e6de8f3f2d9edce6e22e72cbf1d5a337
ricotandrio@gmail.com <ricotandrio@gmail.com>
2024-05-19 22:43:29 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./mejabelajar-web/",
+  base: "/",
   resolve: {
     alias: {
       "@public": path.resolve(__dirname, "./public"),
