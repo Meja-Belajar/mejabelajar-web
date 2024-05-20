7b9f50b82fc808b528342e26ad7916a12d8b317d
ricotandrio@gmail.com <ricotandrio@gmail.com>
2024-05-20 08:02:51 +0700

    fix deployment gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 import store from "./redux/store.ts";
 import { NextUIProvider } from "@nextui-org/react";
@@ -12,9 +12,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     {/* <React.StrictMode> */}
     <NextUIProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Router />
-      </BrowserRouter>
+      </HashRouter>
     </NextUIProvider>
     {/* </React.StrictMode> */}
   </Provider>,
